05b2e9afa6e6e0320dccfc79d2ea6e3c6110d567
Kevin <kevinalexanderduran8@gmail.com>
2022-09-12 03:12:32 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Nav from './Nav/Nav.js'
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav/>
     </div>
   );
 }
---
+++ src/Nav/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+
+export default function Nav() {
+  return (
+    <div className="nav">
+        <img alt="logo" className='nav_lgo' src={logo}/>
+    </div>
+  )
+}
