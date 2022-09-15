f29db8c774121cdb7169c58092a4c5a608a1fe8e
Kevin <kevinalexanderduran8@gmail.com>
2022-09-15 05:04:03 -0700

    test

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -2,9 +2,11 @@ import React from 'react'
 import logo from '../assets/logo.png'
 
 export default function Nav() {
+  console.log('test')
   return (
     <div className="nav">
         <img alt="logo" className='nav_lgo' src={logo}/>
+        
     </div>
   )
 }
