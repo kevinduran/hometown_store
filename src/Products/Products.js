b8152bba354692998e484d43002be78227ca9428
Kevin <kevinalexanderduran8@gmail.com>
2022-09-18 21:49:55 -0700

    test

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -7,52 +7,11 @@ function Products() {
     <div className='products_container'>
         <div className='product_item'>
             <img alt="hoodie" src={hoodie} />
-            <h5>hometown hoodie classic</h5>
+            <h3>hometown hoodie classic</h3>
             <h5>$34.99</h5>
             <button>add to cart</button>
         </div>
-        <div className='product_item'>
-            <img alt="hoodie" src={hoodie} />
-            <h5>hometown hoodie classic</h5>
-            <h5>$34.99</h5>
-            <button>add to cart</button>
-        </div>
-        <div className='product_item'>
-            <img alt="hoodie" src={hoodie} />
-            <h5>hometown hoodie classic</h5>
-            <h5>$34.99</h5>
-            <button>add to cart</button>
-        </div>
-        <div className='product_item'>
-            <img alt="hoodie" src={hoodie} />
-            <h5>hometown hoodie classic</h5>
-            <h5>$34.99</h5>
-            <button>add to cart</button>
-        </div>
-        <div className='product_item'>
-            <img alt="hoodie" src={hoodie} />
-            <h5>hometown hoodie classic</h5>
-            <h5>$34.99</h5>
-            <button>add to cart</button>
-        </div>
-        <div className='product_item'>
-            <img alt="hoodie" src={hoodie} />
-            <h5>hometown hoodie classic</h5>
-            <h5>$34.99</h5>
-            <button>add to cart</button>
-        </div>
-        <div className='product_item'>
-            <img alt="hoodie" src={hoodie} />
-            <h5>hometown hoodie classic</h5>
-            <h5>$34.99</h5>
-            <button>add to cart</button>
-        </div>
-        <div className='product_item'>
-            <img alt="hoodie" src={hoodie} />
-            <h5>hometown hoodie classic</h5>
-            <h5>$34.99</h5>
-            <button>add to cart</button>
-        </div>
+       
     </div>
   )
 }
