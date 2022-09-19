e3f871e54d83122de0e952ed19123992c2516a8e
Kevin <kevinalexanderduran8@gmail.com>
2022-09-18 21:15:57 -0700

    test

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -6,7 +6,9 @@ function Products() {
   return (
     <div className='products_container'>
         <div className='product_item'>
+            <h5>hometown hoodie classic</h5>
             <img alt="hoodie" src={hoodie} />
+            <button>add to cart</button>
         </div>
         <div className='product_item'>
             <img alt="hoodie" src={hoodie} />
