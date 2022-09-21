eb51aaeedd13199c7b5cc06ebb4eecf6d99b0179
Kevin <kevinalexanderduran8@gmail.com>
2022-09-21 06:34:29 -0700

    test

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -3,16 +3,15 @@ import './Products.css'
 
 function Products(productData) {
     let myProductData= productData.productData;
-    
+
   return (
     <div className='products_container'>
         
         {myProductData.map((item)=>{
             console.log(item.imageLink)
-            
             return(
             <div className='product_item' key={item.itemnum}>
-                <img alt='' src={`../assets/${item.imageLink}`} />
+                <img alt={item.itemName} src={item.imageLink} />
                 <h3>{item.itemName}</h3>
                 <h5>{item.price}</h5>
                 <button>view details</button>
