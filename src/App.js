551902c62a107dee583e53dba0c6c6857dd32397
Kevin <kevinalexanderduran8@gmail.com>
2022-09-20 18:43:00 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Nav from './Nav/Nav.js'
 import Products from './Products/Products.js'
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Nav/>
-        <Products/>
-      </div>
-    </Router>
+    <div className="App">
+      <Nav/>
+      <Products/>
+    </div>
   );
 }
 
