896df0022286961753d0e54ca2c433fb1ab07339
Kevin <kevinalexanderduran8@gmail.com>
2022-09-20 18:45:33 -0700

    test

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,31 +1,9 @@
 import React from 'react'
-import hoodie from '../assets/hoodie.png'
 import './Products.css'
 
 function Products() {
   return (
     <div className='products_container'>
-        <div className='product_item'>
-            <img alt="hoodie" src={hoodie} />
-            <h3>hometown hoodie</h3>
-            <h5>$34.99</h5>
-            <button>view details</button>
-            <button>add to cart</button>
-        </div>
-        <div className='product_item'>
-            <img alt="hoodie" src={hoodie} />
-            <h3>hometown hoodie</h3>
-            <h5>$34.99</h5>
-            <button>view details</button>
-            <button>add to cart</button>
-        </div>
-        <div className='product_item'>
-            <img alt="hoodie" src={hoodie} />
-            <h3>hometown hoodie</h3>
-            <h5>$34.99</h5>
-            <button>view details</button>
-            <button>add to cart</button>
-        </div>
         <div className='product_item'>
             <img alt="hoodie" src={hoodie} />
             <h3>hometown hoodie</h3>
